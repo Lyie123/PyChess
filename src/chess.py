7da48b3e2e572fa9376fe460fecf87760632631b
lyie <moch.rene@googlemail.com>
2020-08-31 23:30:59 +0200

    add __str__ and new board representation

--- src/chess.py
+++ src/chess.py
@@ -1,4 +1,3 @@
-from itertools import product
 from enum import Enum
 
 class Color(Enum):
@@ -13,23 +12,39 @@ class Board:
     Represent a chessboard
     A chessboard is represented by a FEN string
     Wiki: https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation
+    White -> upper case
+    Black -> lower case
     """
     fen_starting_position = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
 
     def __init__(self, fen: str = fen_starting_position):
         self._columns = list('ABCDEFGH')
         self._rows = list('12345678')
-        self._board = dict.fromkeys([''.join(n) for n in product(self._columns, self._rows)])
+        self._board = []
+        self.__init_fen(fen)
 
     def __str__(self):
-        board = []
-        board.append(' ' + ''.join(self._columns) + '\n')
-        for row in self._rows:
-            board.append(row)
-            for column in self._columns:
-                board.append(self._board[column+row])
-            board.append('\n')
-        return ''.join(map(lambda x: '-' if x is None else x, board))
+        # replace None with empty character
+        buffer = [n if n is not None else ' ' for n in self._board]
+
+        # chunk list into 8 pieces and join togehter as string
+        return '\n'.join([''.join(buffer[n*8:(n+1)*8]) for n in range(8)])
+
+    def __init_fen(self, fen: str):
+        """
+        Create Board with position dependent of FEN string
+        """
+        fen_sep = fen.split(' ')
+
+        fen_board = fen_sep[0]
+        for n in fen_board:
+            if n == '/':
+                pass
+            elif n.isnumeric():
+                for pawn in range(int(n)):
+                    self._board.append(None)
+            else:
+                self._board.append(n)
 
 class Figure:
     """
